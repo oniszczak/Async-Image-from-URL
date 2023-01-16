c3675e0da3ab812c2429ec5fd3d961c9421b4cd2
oniszczak <aleks42@gmail.com>
2023-01-15 21:49:52 -0500

    Initial Commit

---
+++ Async Image from URL/Async_Image_from_URLApp.swift
@@ -0,0 +1,17 @@
+//
+//  Async_Image_from_URLApp.swift
+//  Async Image from URL
+//
+//  Created by ALEKSANDER ONISZCZAK on 2023-01-15.
+//
+
+import SwiftUI
+
+@main
+struct Async_Image_from_URLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
